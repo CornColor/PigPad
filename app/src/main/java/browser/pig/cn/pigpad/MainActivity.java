5a782ac25b40d3b65379c0b28cbc15bd7650d9e2
Liuyutao <liuyutao@yuanhuiit.com>
2020-01-13 15:04:02 +0800

    xm

---
+++ app/src/main/java/browser/pig/cn/pigpad/MainActivity.java
@@ -0,0 +1,13 @@
+package browser.pig.cn.pigpad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
