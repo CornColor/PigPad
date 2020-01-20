58ee76b3ae301d9b1862f28ad06b06da13f06ecc
Liuyutao <liuyutao@yuanhuiit.com>
2020-01-20 17:36:41 +0800

    444

--- app/src/main/java/browser/pig/cn/pigpad/db/DbHelper.java
+++ app/src/main/java/browser/pig/cn/pigpad/db/DbHelper.java
@@ -1,16 +1,21 @@
-package cn.my.library.utils.db;
+package browser.pig.cn.pigpad.db;
 
 import android.content.Context;
 
-
 import org.greenrobot.greendao.AbstractDao;
+
+import browser.pig.cn.pigpad.bean.GoodsBean;
+import browser.pig.cn.pigpad.bean.StepABean;
 
 
 public class DbHelper {
     private static final String DB_NAME = "woosport.db";//数据库名称
     private static DbHelper instance;
-
-
+    private DaoMaster.DevOpenHelper mHelper;
+    private DaoMaster mDaoMaster;
+    private DaoSession mDaoSession;
+    private static DBManager<GoodsBean,Long> goodsBeanLongDBManager;
+    private static DBManager<StepABean,Long> stepABeanLongDBManager;
     private DbHelper() {
 
     }
@@ -26,13 +31,41 @@ public class DbHelper {
         return instance;
     }
 
-    public void init(Context context) {
+    public void init(Context context,String dbName){
+        mHelper = new DaoMaster.DevOpenHelper(context,dbName,null);
+        mDaoMaster = new DaoMaster(mHelper.getWritableDatabase());
+        mDaoSession =  mDaoMaster.newSession();
 
     }
 
-    public void init(Context context, String dbName) {
+    public void init(Context context){
+        mHelper = new DaoMaster.DevOpenHelper(context,DB_NAME,null);
+        mDaoMaster = new DaoMaster(mHelper.getWritableDatabase());
+        mDaoSession =  mDaoMaster.newSession();
 
     }
 
+    public DBManager<GoodsBean,Long> goodsBeanLongDBManager(){
+        if(goodsBeanLongDBManager == null){
+            goodsBeanLongDBManager = new DBManager<GoodsBean, Long>() {
+                @Override
+                public AbstractDao<GoodsBean, Long> getAbstractDao() {
+                    return mDaoSession.getGoodsBeanDao();
+                }
+            };
+        }
+        return goodsBeanLongDBManager;
+    }
 
+    public DBManager<StepABean,Long> stepABeanLongDBManager(){
+        if(stepABeanLongDBManager == null){
+            stepABeanLongDBManager = new DBManager<StepABean, Long>() {
+                @Override
+                public AbstractDao<StepABean, Long> getAbstractDao() {
+                    return mDaoSession.getStepABeanDao();
+                }
+            };
+        }
+        return stepABeanLongDBManager;
+    }
 }
